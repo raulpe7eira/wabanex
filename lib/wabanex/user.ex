149934c55942feb31ecb2c5d980bc1651f832cc3
Raul Pereira <raul.pereira@gmail.com>
2021-12-18 20:19:51 -0300

    class #2 - add user repo

---
+++ lib/wabanex/user.ex
@@ -0,0 +1,26 @@
+defmodule Wabanex.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @fields [:name, :email, :password]
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_length(:name, min: 2)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> unique_constraint([:email])
+  end
+end
