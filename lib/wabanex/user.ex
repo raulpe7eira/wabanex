ee992515ae0f9a5f92de468ace153f7f74352548
Raul Pereira <raul.pereira@gmail.com>
2021-12-18 23:58:31 -0300

    class #4 - add relation user <> training

--- lib/wabanex/user.ex
+++ lib/wabanex/user.ex
@@ -2,6 +2,8 @@ defmodule Wabanex.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Wabanex.Training
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
   @fields [:name, :email, :password]
@@ -10,6 +12,8 @@ defmodule Wabanex.User do
     field :name, :string
     field :email, :string
     field :password, :string
+
+    has_one :training, Training
 
     timestamps()
   end
