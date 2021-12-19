932de5e9f73b677b20f74036a0c0b8057c9da241
Raul Pereira <raul.pereira@gmail.com>
2021-12-19 00:18:46 -0300

    class #4 - add mutation createTraining

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -3,14 +3,25 @@ defmodule WabanexWeb.Schema.Types.Root do
 
   alias Crudry.Middlewares.TranslateErrors
   alias WabanexWeb.Resolvers.User, as: UserResolver
+  alias WabanexWeb.Resolvers.Training, as: TrainingResolver
+  alias WabanexWeb.Schema.Types
 
-  import_types WabanexWeb.Schema.Types.User
+  import_types Types.Custom.UUID4
+  import_types Types.User
+  import_types Types.Training
 
   object :root_mutation do
     field :create_user, type: :user do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware TranslateErrors
+    end
+
+    field :create_training, type: :training do
+      arg :input, non_null(:create_training_input)
+
+      resolve &TrainingResolver.create/2
       middleware TranslateErrors
     end
   end
